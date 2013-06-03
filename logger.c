dca1d60b93c3b896257c7bed3a552e260040e9f1
Dennis Boone <drb@msu.edu>
2013-06-02 23:28:13 -0400

    Fix date stamp on log messages; stupid formatting, y2k issue are gone.

--- logger.c
+++ logger.c
@@ -74,8 +74,8 @@ local_time()
 
 	time(&clock);		/* Get the current time */
 	tm = localtime(&clock);
-	sprintf(TimeBuff, "%02d/%02d/%02d %02d:%02d:%02d",
-		tm->tm_mday, (tm->tm_mon + 1), tm->tm_year,
+	sprintf(TimeBuff, "%04d-%02d-%02d %02d:%02d:%02d",
+		tm->tm_year+1900, (tm->tm_mon + 1), tm->tm_mday,
 		tm->tm_hour, tm->tm_min, tm->tm_sec);
 	return TimeBuff;
 }
